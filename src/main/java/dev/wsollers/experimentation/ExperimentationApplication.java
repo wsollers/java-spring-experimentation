7668db9f184c9eb863374a5f90feaa6ed8422e45
BoondockTaints <wsollers@gmail.com>
2023-12-23 11:33:55 -0500

    Config built
    config built

---
+++ src/main/java/dev/wsollers/experimentation/ExperimentationApplication.java
@@ -0,0 +1,13 @@
+package dev.wsollers.experimentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExperimentationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/wsollers/experimentation/ExperimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wsollers.experimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
